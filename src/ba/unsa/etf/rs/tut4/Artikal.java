c60c91d4d7d9c4fc674921562d7bca02958d2c16
Mirza Smrkovic <msmrkovic1@etf.unsa.ba>
2020-03-26 18:06:43 +0100

    dodaj JavaBeans specifikaciju i konstruktor za klasu Artikal

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -1,5 +1,22 @@
 package ba.unsa.etf.rs.tut4;
 
 public class Artikal {
+  private String sifra, naziv;
+  private double cijena;
 
+    Artikal(String str){
+      String[] odvoji = str.split(",");
+      this.sifra = odvoji[0];
+      this.naziv = odvoji[1];
+      this.cijena = Double.parseDouble(odvoji[2]);
+
+    }
+
+  public double getCijena() { return cijena; }
+  public String getNaziv() { return naziv; }
+  public String getSifra() { return sifra; }
+
+  public void setCijena(double cijena) { this.cijena = cijena; }
+  public void setSifra(String sifra) { this.sifra = sifra; }
+  public void setNaziv(String naziv) { this.naziv = naziv; }
 }
