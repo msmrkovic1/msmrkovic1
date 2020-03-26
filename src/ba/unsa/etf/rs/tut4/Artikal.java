9064030498cd936df6041f341f6571ba0ee95ba4
Mirza Smrkovic <msmrkovic1@etf.unsa.ba>
2020-03-26 18:28:11 +0100

    dodaj metode toString i izbaciDuplikate u Artikal.java klasu

--- src/ba/unsa/etf/rs/tut4/Artikal.java
+++ src/ba/unsa/etf/rs/tut4/Artikal.java
@@ -1,16 +1,17 @@
 package ba.unsa.etf.rs.tut4;
 
+import java.util.ArrayList;
+
 public class Artikal {
   private String sifra, naziv;
   private double cijena;
 
-    Artikal(String str){
-      String[] odvoji = str.split(",");
-      this.sifra = odvoji[0];
-      this.naziv = odvoji[1];
-      this.cijena = Double.parseDouble(odvoji[2]);
-
-    }
+  Artikal(String str){
+    String[] odvoji = str.split(",");
+    this.sifra = odvoji[0];
+    this.naziv = odvoji[1];
+    this.cijena = Double.parseDouble(odvoji[2]);
+  }
 
   public double getCijena() { return cijena; }
   public String getNaziv() { return naziv; }
@@ -19,4 +20,14 @@ public class Artikal {
   public void setCijena(double cijena) { this.cijena = cijena; }
   public void setSifra(String sifra) { this.sifra = sifra; }
   public void setNaziv(String naziv) { this.naziv = naziv; }
+  
+  public String toString(){ return sifra + ", " + naziv + ", " + cijena; }
+  public static ArrayList<Artikal> izbaciDuplikate(ArrayList<Artikal> artikli){
+    for(int i = 0; i < artikli.size(); i++){
+      for(int j = i + 1; j < artikli.size(); j++){
+        if(a.get(i).equals(artikli.get(j))) artikli.remove(j);
+      }
+    }
+    return a;
+  }
 }
