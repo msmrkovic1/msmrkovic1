223dddfe05e9e125751e004860d670546b74f696
Mirza Smrkovic <msmrkovic1@etf.unsa.ba>
2020-03-27 22:39:22 +0100

    dodaj funkcionalnost unosa artikala kroz interfejs

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -1,5 +1,24 @@
 package ba.unsa.etf.rs.tut4;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import java.util.ArrayList;
 
+public class Controller {
+  public TextArea upisArtikala;
+  public TextArea ispisArtikala;
+  public Button dodajArtikle;
+  
+  ArrayList<Artikal> Artikli = new ArrayList<Artikal>();
+  public void UpisiArtikle(ActionEvent actionEvent) {
+    String upis = upisArtikala.getText();
+    String[] artikli = upis.split('\n');
+    for (String artikal : artikli) Artikli.add(new Artikal(artikal))
+    Artikli = Artikal.izbaciDuplikate(Artikli);
+  }
 }
