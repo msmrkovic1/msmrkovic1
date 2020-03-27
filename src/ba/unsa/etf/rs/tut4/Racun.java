d6ad6f3f1477e731350cbe4659bc4d4de53483eb
Mirza Smrkovic <msmrkovic1@etf.unsa.ba>
2020-03-27 21:35:32 +0100

    dodaj atribut privatnu klasu Stavka i njene getter/setter

--- src/ba/unsa/etf/rs/tut4/Racun.java
+++ src/ba/unsa/etf/rs/tut4/Racun.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rs.tut4;
 
+import java.util.ArrayList;
+
 public class Racun {
+  public Racun() { Artikli = new ArrayList<>(); }
+  public Racun(Artikal artikal, int kol) {
+    this();
+    Artikli.add(new Stavka(artikal, kol));
+  }
+  
+  private class Stavka {
+    private Artikal artikal;
+    private int kolicina;
+    
+    public Stavka(Artikal artikal, int kolicina) {
+      this.artikal = artikal; this.kolicina = kolicina;
+    }
+  
+    // Setteri
+    public void setArtikal(Artikal artikal) { this.artikal = artikal; }
+    public void setKolicina(int kolicina) { this.kolicina = kolicina; }
+    // Getteri
+    public Artikal getArtikal() { return artikal; }
+    public int getKolicina() { return kolicina; }
+    
+  }
 }
+
